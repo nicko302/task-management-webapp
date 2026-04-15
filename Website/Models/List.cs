27d95fdf8aa472b84965236de7ce9a5e81939d3d
nicko <nickrev55@gmail.com>
2026-04-15 19:47:56 +0100

    now displays colour options when editing a list
    - not functioning yet though :(

--- Website/Models/List.cs
+++ Website/Models/List.cs
@@ -15,5 +15,18 @@ namespace CRUD_Application.Models
 
         public Board Board { get; set; } // Reference navigation
         public virtual ICollection<Task> Tasks { get; set; } = new List<Task>(); // Reference navigation
+
+        public string HexColour => Colour?.ToLower() switch
+        {
+            "red" => "#451b12",
+            "green" => "#103821",
+            "blue" => "#0d365c",
+            "orange" => "#a1620b",
+            "purple" => "#4d2d47",
+            "teal" => "#1d524b",
+            "brown" => "#3d332a",
+            "yellow" => "#edd83b",
+            _ => "#333333"
+        };
     }
 }
